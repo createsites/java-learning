ede6cd6c77bfc281f2673714e96a94e87c1b91cd
Alex Scrobot <scrobot91@gmail.com>
2017-08-10 22:53:44 +0700

    first commit

---
+++ src/sprint1/Sortable.java
@@ -0,0 +1,13 @@
+package sprint1;
+
+/**
+ * Created by aleksejskrobot on 10.08.17.
+ */
+public interface Sortable {
+
+    void quick();
+    void bubble();
+    void simple();
+    void merge();
+
+}
