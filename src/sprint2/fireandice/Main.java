1554a2b5f9244dd2903cdf299425b1edb898fcf1
a.novoselov <admin@createsites.net>
2017-08-16 16:10:51 +0700

    Tests for exception part.

--- src/sprint2/fireandice/Main.java
+++ src/sprint2/fireandice/Main.java
@@ -1,16 +1,16 @@
 package sprint2.fireandice;
 
-/*import com.google.gson.Gson;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import sprint2.fireandice.models.Sample;*/
+import sprint2.fireandice.models.Sample;
 
 public class Main {
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         Gson gson = new GsonBuilder().create();
         Sample ex = gson.fromJson(Repository.sampleJson(), Sample.class);
 
         System.out.println(ex.toString());
-    }*/
+    }
 
 }
--- test/Sprint2Test.java
+++ test/Sprint2Test.java
@@ -1,48 +1,121 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
 import sprint2.collections.ArrayIteratorViewer;
 import sprint2.collections.interfaces.IViewer;
+import sprint2.exceptions.ArrayTransform;
 import sprint2.exceptions.MyArrayDataException;
 import sprint2.exceptions.MySizeArrayException;
 import sprint2.fireandice.Repository;
 import sprint2.fireandice.models.Sample;
 
+@RunWith(DataProviderRunner.class)
 public class Sprint2Test {
     @Rule
     public TestName name = new TestName();
 
-    @Test
-    public void assertTest() {
-        System.out.println("Test of " + name.getMethodName() + ": OK");
-        Assert.assertEquals(true, true);
+    // правильные массивы для теста 4х4
+    @DataProvider
+    public static Object[][] fourByfourArrayCorrectProvider() {
+
+        return new Object[][] {
+            {
+                136, // sum
+                new String[][]{
+                        {"1", "2", "3", "4"},
+                        {"5", "6", "7", "8"},
+                        {"9", "10", "11", "12"},
+                        {"13", "14", "15", "16"}
+                },
+            },
+            {
+                440, // sum
+                new String[][] {
+                    {"11", "12", "13", "14"},
+                    {"21", "22", "23", "24"},
+                    {"31", "32", "33", "34"},
+                    {"41", "42", "43", "44"}
+                },
+            },
+        };
+    }
+
+    // массивы неправильного размера для теста 4х4
+    @DataProvider
+    public static Object[][] fourByfourArrayIncorrectSizeProvider() {
+
+        return new Object[][] {
+            {
+                new String[][] {
+                    {"11", "12", "13", "14"},
+                    {"21", "22", "23", "24"},
+                    {"31", "32", "33", "34"}
+                }
+            },
+            {
+                new String[][] {
+                    {"11", "12", "13"},
+                    {"21", "22", "23", "24"},
+                    {"31", "32", "33", "34"},
+                    {"41", "42", "43", "44"}
+                },
+            }
+        };
+    }
+
+    // массивы с неправильными данными для теста 4х4
+    @DataProvider
+    public static Object[][] fourByfourArrayIncorrectDataProvider() {
+
+        return new Object[][] {
+            {
+                new String[][] {
+                    {"11", "string!", "13", "14"},
+                    {"21", "22", "23", "24"},
+                    {"31", "32", "33", "34"},
+                    {"31", "32", "33", "34"}
+                }
+            },
+            {
+                new String[][] {
+                    {"11", "12", "13", "14"},
+                    {"21", "22", "23", "24"},
+                    {"31", "32", "string!", "34"},
+                    {"41", "42", "43", "44"}
+                },
+            }
+        };
     }
 
     @Test
-    public void fourByfourArrayTest() {
+    @UseDataProvider("fourByfourArrayCorrectProvider")
+    // TODO здесь throws писать надо ли?
+    public void fourByfourArraySumTest(int sum, String[][] arrData) throws MyArrayDataException, MySizeArrayException {
         System.out.println("Test of " + name.getMethodName() + ": OK");
-        // TODO: implement
-    }
-
-    @Test
-    public void fourByfourArraySumTest() {
-        System.out.println("Test of " + name.getMethodName() + ": OK");
-        // TODO: implement
+        Assert.assertEquals(sum, new ArrayTransform().getSum(arrData));
     }
 
     @Test(expected = MySizeArrayException.class)
-    public void sizeArrayTest() {
+    @UseDataProvider("fourByfourArrayIncorrectSizeProvider")
+    // TODO здесь throws писать надо ли?
+    public void fourByfourArraySizeTest(String[][] arrData) throws MyArrayDataException, MySizeArrayException {
         System.out.println("Test of " + name.getMethodName() + ": OK");
-        // TODO: implement
+        new ArrayTransform().getSum(arrData);
     }
 
     @Test(expected = MyArrayDataException.class)
-    public void arrayDataTest() {
+    @UseDataProvider("fourByfourArrayIncorrectDataProvider")
+    // TODO здесь throws писать надо ли?
+    public void fourByfourArrayDataTest(String[][] arrData) throws MyArrayDataException, MySizeArrayException {
         System.out.println("Test of " + name.getMethodName() + ": OK");
-        // TODO: implement
+        new ArrayTransform().getSum(arrData);
     }
 
     @Test
